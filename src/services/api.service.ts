7eda71c48ae1d769c7594bf9bf61c635aed8fd89
Alexandr Semenec <alexandrsemenec01@gmail.com>
2024-07-08 12:10:32 +0300

    task 4 done

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,22 +1,19 @@
 import axios from "axios";
 
-let axiosInstance = axios.create({
-    baseURL: "https://dummyjson.com",
-
-});
-
-axiosInstance.interceptors.request.use(request => {
-    request.headers.set("Content-Type", "application/json");
-    return request
-})
-
+let axiosInstance = axios.create(
+    {
+        baseURL: "https://dummyjson.com",
+        headers: {
+            'ContentType': "application/json"
+        }
+    }
+);
 const getAllUsers = async () => {
     return await axiosInstance.get("/users").then(res => res.data.users);
 
 }
 const getPostsOfUsers = async (id: number) => {
-    const url = `/users/${id}/posts/`
-    await axiosInstance.get(url).then(res => res.data.posts)
+    return await axiosInstance.get("/users/" + id + "/posts/").then(res => res.data.posts);
 }
 
 export {getAllUsers, getPostsOfUsers}
--- src/services/post-component/PostsComponent.tsx
+++ src/services/post-component/PostsComponent.tsx
@@ -7,15 +7,25 @@ type IPostsProps = {
 type Props = {
     posts: IPosts[];
 }
-class PostsComponent extends Component< Props, IPostsProps> {
+
+class PostsComponent extends Component<Props, IPostsProps> {
     constructor(props: Props) {
         super(props)
         this.state = {
-            posts: props.posts
+            posts: this.props.posts
         };
     }
+
+    componentDidUpdate(prevProps: Props) {
+        if (prevProps.posts !== this.props.posts) {
+            this.setState({
+                posts: this.props.posts
+            });
+        }
+    }
+
     render() {
-        // this.state.posts.map((value, index) => console.log(value))
+        // this.state.posts.map((value) => console.log(value))
         return (
             <ul>
                 {this.state.posts.map((value) => <li key={value.id}>{value.userId}: {value.body}</li>)}
