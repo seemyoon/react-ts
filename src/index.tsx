766d4159ab9d5a583bd47c23cf3a01961b0fc178
Alexandr Semenec <alexandrsemenec01@gmail.com>
2024-07-14 21:55:16 +0300

    task 7 create pages

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import UsersPage from "./pages/UsersPage";
+import PostsPage from "./pages/PostsPage";
+import CommentsPage from "./pages/CommentsPage";
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 const router = createBrowserRouter([
-    {path: ""}
+    {path: "", element: <UsersPage/>},
+    {path: "", element: <PostsPage/>},
+    {path: "", element: <CommentsPage/>},
 ])
 root.render(
 
