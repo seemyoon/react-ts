eac234cb0021e34045b909ccb564cf5a41889f49
Alexandr Semenec <alexandrsemenec01@gmail.com>
2024-07-18 22:40:57 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import ChildComponent from "./components/ChildComponent";
+import MenuComponent from "./components/MenuComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+// const App = () => {
+//     const [value, dispatch] = useReducer((prevState: number, action: { type: string; payload: number }) => {
+//         switch (action.type) {
+//             case 'add':
+//                 return prevState + action.payload;
+//             case "remove":
+//                 return prevState - action.payload;
+//             default:
+//                 return prevState;
+//         }
+//     }, 0)
+//     return (
+//         <div>
+//             <button onClick={() => {
+//                 dispatch({type: "remove", payload: 1})
+//             }}>dec
+//             </button>
+//
+//             <button onClick={() => {
+//                 dispatch({type: "add", payload: 1})
+//             }}>inc
+//             </button>
+//             <br/>
+//             <h2>state: {value}</h2>
+//         </div>
+//     );
+// }
+// const App = () => {
+//     // stay lifting with func + property drills
+//     const [letters, setLetters] = useState("")
+//     const addLetter = ()=>{
+//         setLetters("A")
+//     }
+//     console.log(letters)
+//     return (
+//
+//     <UserComponent letters={letters} func={addLetter} />)
+// }
+// export default App;
+
+// const App = () => {
+//     unsubscribe function in useEffect()
+//     const [counter, setCounter] = useState(0)
+//     useEffect(() => {
+//         const value = setInterval(()=>{
+//             console.log("any")
+//         }, 1000)
+//       return ()=>{
+//             clearInterval(value)
+//       }
+//     }, [counter]);
+//     return (
+//         <button onClick={()=>{
+//             setCounter(counter + 1)
+//         }}>Click here! {counter}</button>
+//     );
+// };
+// export default App;
+// https://jsonplaceholder.typicode.com/posts/1
+
+// const useFetch = <T, >(url: string) => {
+//     const [data, setData] = useState<T[]>([])
+//
+//     useEffect(() => {
+//         fetch("https://jsonplaceholder.typicode.com/posts/1" + url)
+//             .then(response => response.json())
+//             .then(value => setData([...value]))
+//
+//     }, []);
+//     return data
+// }
+// const App = () => {
+//     const res = useFetch<{ id: number, body: string }>("/comments")
+//     return (
+//         <div>
+//             {res.map(value => (<li key={value.id}>{value.id}{value.body}</li>))}
+//         </div>
+//     );
+// };
+// export default App;
+
+
+const App = () => {
+// memo
+    const [id, setId] = useState<number>(1)
+console.log("app render")
+    const incrementID = () => {
+        setId(id => id + 1)
+    }
+
+    return (<div>
+            <MenuComponent/>
+            <ChildComponent id={id}/>
+            <button onClick={incrementID}>Click here</button>
+        </div>
+
+    );
+};
 
 export default App;
+// useMemo
+// useCallback
---
+++ src/components/MenuComponent.tsx
@@ -0,0 +1,18 @@
+import React, {memo} from 'react';
+
+const MenuComponent = memo(() => {
+    console.log("Menu")
+    return (
+        <div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+        </div>
+    );
+});
+
+export default MenuComponent;
